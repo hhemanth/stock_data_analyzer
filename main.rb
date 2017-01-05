c1f73b31fa45811f9b2b241030c6a297c22843f1
hhemanth <h.hemanth@jifflenow.com>
2017-01-05 12:40:44 +0530

    adding correct time to logs

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative './StockStreamsAnalyzer'
 require_relative './StockDataDownloader'
 
 def main
-  p "Starting downloading Historical stock data"
+  p "Starting downloading Historical stock data @ #{Time.now.localtime("+05:30").to_s}" 
   p "*************" * 5
   sdd = StockDataDownloader.new
   sdd.download_stock_data
